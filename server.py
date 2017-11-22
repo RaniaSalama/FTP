e58dc18d282042496802b06c3277a1f87c1a2df8
RaniaSalama <rania.ibrahim.salama@gmail.com>
2017-11-22 02:10:05 +0000

    initial structure

--- server.py
+++ server.py
@@ -3,10 +3,39 @@ from server_class import Server
 import sys
 import threading
 
+USERS_FILE = 'users.csv'
+
+# Key = username.
+# Value = password.
+users = {}
+
+# Key = client_IP:client_port.
+# Value = state number.
+# State 0 --> need authorization.
+# State 1 --> provided username and still need to provide password.
+# State 2 --> authorized.
+current_users_states = {}
+
+# Key =  client_IP:client_port.
+# Value = provided username if any.
+current_users_username = {}
+
+	
+# Load users names and passwords.
+def load_users(filename):
+    csvfile = open(filename,'rb')
+    lines = csv.reader(csvfile, delimiter = ',')
+    for line in lines:
+        users[line[0]] = line[1]
+
 # Connection thread to handle each client response.
 # connectionSocket: the socket with the client.
 def connection_thread(connectionSocket):
     sentence = connectionSocket.recv(1024).decode()
+    # Authorize here
+    # ..............................
+    
+    # Call server for list, store and reterive operations.
     server = Server()
     response = server.send_ftp_response(sentence, connectionSocket)
     connectionSocket.close()
@@ -17,9 +46,12 @@ serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 
+# Load users in the system.
+load_users(USERS_FILE)
+
 print 'The server is ready to receive'
 
 while True:
     connectionSocket, addr = serverSocket.accept()
-	# Handle each client request in a seperated thread.
+    # Handle each client request in a seperated thread.
     threading.Thread(target=connection_thread, args=(connectionSocket,)).start()
--- server_class.py
+++ server_class.py
@@ -1,8 +1,21 @@
 import os, inspect, stat
 from datetime import datetime
+import csv
 
 class Server:
+	users = {}
+	
+	# Load users names and passwords.
+	def load_users(self, filename):
+		csvfile = open(filename,'rb')
+		lines = csv.reader(csvfile, delimiter = ',')
+		for line in lines:
+			users[line[0]] = line[1]
+	
+
 	# Construct and send the ftp response.
 	# sentence: the request message.
 	# connectionSocket: the socket to send the response through.
-    def send_ftp_response(self, sentence, connectionSocket):
+	def send_ftp_response(self, sentence, connectionSocket):
+		print('here')
+		
