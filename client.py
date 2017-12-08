c69a6862a291a15a5677c8f01ebd01489bac147e
Ahmed Elbagoury <am.elbagoury@gmail.com>
2017-12-07 19:35:11 -0500

    Wrapping code in main functions in client.py and server.py. Making params in one command. parse the port number in port command in int

--- client.py
+++ client.py
@@ -14,56 +14,68 @@ def is_valid_response(response):
     elif response.startswith('332'):
         return False
     return True
-
-server_name = sys.argv[1]
-server_port = int(sys.argv[2])
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect((server_name,server_port))
-request = 'START...'
-print('send start request first!')
-send_request(client_socket, request)
-# Start authentication.
-username = raw_input('Enter your username: ')
-request = 'USER ' + username
-send_request(client_socket, request)
-password = raw_input('Enter your password: ')
-request = 'PASS ' + password
-response = send_request(client_socket, request)
-if not is_valid_response(response):
-    print('Wrong username and/or password!')
-    print('Goodbye!')
-    client_socket.close()
-    exit(0)
-
-# User is authenticated. Start file commands.
-data_socket = 0
-while True:
-    print('Please enter LIST, PORT, RETE, STOR or QUIT')
-    command = raw_input('Enter your command: ')
-    if command == 'QUIT':
-        print('Goodbye')
+if __name__ == '__main__':
+    server_name = sys.argv[1]
+    server_port = int(sys.argv[2])
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((server_name,server_port))
+    request = 'START...'
+    print('send start request first!')
+    send_request(client_socket, request)
+    # Start authentication.
+    username = raw_input('Enter your username: ')
+    request = 'USER ' + username
+    send_request(client_socket, request)
+    password = raw_input('Enter your password: ')
+    request = 'PASS ' + password
+    response = send_request(client_socket, request)
+    if not is_valid_response(response):
+        print('Wrong username and/or password!')
+        print('Goodbye!')
         client_socket.close()
         exit(0)
-    elif command == 'PORT':
-        port_number = raw_input('Enter data port number: ')
-        request = 'PORT ' + port_number
-        send_request(client_socket, request)
-        # list on the data connection.
-        data_socket = socket(AF_INET,SOCK_STREAM)
-        data_socket.bind(('', int(port_number)))
-        data_socket.listen(1)
-    elif command == 'LIST':
-        request = 'LIST'
-        send_request(client_socket, request)
-        print('before accept')
-        data_connection_socket, server_addr = data_socket.accept()
-        print('after accept')
-        response = data_connection_socket.recv(1024)
-        print(response)
-    elif command == 'RETE':
-        print('In reterive!')
-    elif command == 'STOR':
-        print('In store!')
-    else:
-        print('Invalid command!')
+
+    # User is authenticated. Start file commands.
+    data_socket = 0
+    while True:
+        print('Please enter LIST, PORT, GET, STOR or QUIT')
+        input_str = raw_input('Enter your command: ').split()
+        command = input_str[0].lower()
+        if command == 'quit':
+            print('Goodbye')
+            client_socket.close()
+            exit(0)
+        elif command == 'port':
+            if len(input_str) < 2:
+                print 'Error please enter port <port number>'
+                continue
+            port_number = 0
+            try:
+                port_number = int(input_str[1])
+            except ValueError:
+                print 'Error please enter port <port number>'
+                continue
+            request = 'PORT ' + str(port_number)
+            send_request(client_socket, request)
+            # list on the data connection.
+            data_socket = socket(AF_INET,SOCK_STREAM)
+            data_socket.bind(('', int(port_number)))
+            data_socket.listen(1)
+        elif command == 'list':
+            request = 'LIST'
+            send_request(client_socket, request)
+            print('before accept')
+            data_connection_socket, server_addr = data_socket.accept()
+            print('after accept')
+            response = data_connection_socket.recv(1024)
+            print(response)
+        elif command == 'get':
+            if len(input_str) < 2:
+                print 'Error please enter get <file name>'
+                continue
+            print('In reterive!'), input_str
+        elif command == 'STOR':
+            print('In store!')
+        else:
+            print('Invalid command!')
 
--- server.py
+++ server.py
@@ -3,27 +3,27 @@ from server_class import Server
 import sys
 import threading
 
+if __name__ == '__main__':
+	# Connection thread to handle each client response.
+	# connectionSocket: the socket with the client.
+	def connection_thread(connection_socket, server, client_addr):
+	    sentence = connection_socket.recv(1024).decode()
+	    response = server.send_ftp_response(sentence, connection_socket, client_addr)
+	    connection_socket.close()
 
-# Connection thread to handle each client response.
-# connectionSocket: the socket with the client.
-def connection_thread(connection_socket, server, client_addr):
-    sentence = connection_socket.recv(1024).decode()
-    response = server.send_ftp_response(sentence, connection_socket, client_addr)
-    connection_socket.close()
+	# Get the args from the commandline.
+	server_port = int(sys.argv[1])
+	server_socket = socket(AF_INET,SOCK_STREAM)
+	server_socket.bind(('', server_port))
+	server_socket.listen(1)
 
-# Get the args from the commandline.
-server_port = int(sys.argv[1])
-server_socket = socket(AF_INET,SOCK_STREAM)
-server_socket.bind(('', server_port))
-server_socket.listen(1)
+	print 'The server is ready to receive'
 
-print 'The server is ready to receive'
+	server = Server()
+	server.load_users()
 
-server = Server()
-server.load_users()
-
-while True:
-    connection_socket, client_addr = server_socket.accept()
-    # Handle each client request in a seperated thread.
-    threading.Thread(target=connection_thread, args=(connection_socket,
-                                                     server, client_addr, )).start()
+	while True:
+	    connection_socket, client_addr = server_socket.accept()
+	    # Handle each client request in a seperated thread.
+	    threading.Thread(target=connection_thread, args=(connection_socket,
+	                                                     server, client_addr, )).start()
